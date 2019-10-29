08346265318976ac92e7814008113eedb2b1bbc3
Milan Capoor <capoorm123@gmail.com>
2019-10-28 23:24:08 -0400

    slight accuracy improvement

--- model.py
+++ model.py
@@ -7,12 +7,12 @@ def nonlin(X, deriv = False):
         return 1 / (1 + np.exp(-X))
 
 def init_data():
-    X = np.array([ [1, 1], 
+    X = np.array([ [1, 1],
                    [0, 1],
                    [1, 0],
                    [0, 0] ])
 
-    Y = np.array([ [0], 
+    Y = np.array([ [0],
                    [1],
                    [1],
                    [0] ])
@@ -32,24 +32,26 @@ def update(X, w, Y = init_data()[1]):
 
     return delta_w
 
-def process(input, weights):
+def process(input, weights, delta=0):
     net = input * weights
     out = nonlin(net)
-    delta = input * update(input, weights)
+    delta += input * update(input, weights)
     updated_weights = weights + delta
 
-    return out, updated_weights
+    return out, updated_weights, delta
 
 X, Y, w = init_data()
-for epoch in range(10000):
+for epoch in range(100000):
     initial_input = X 
     initial_weights = w
 
-    l1_output, l1_weights = process(initial_input, initial_weights)
-    h1_output, h1_weights = process(l1_output, l1_weights)
-    h2_output, h2_weights = process(h1_output, h1_weights)
-    output = nonlin(h2_output * h2_weights)
+    l1_output, l1_weights, l1_delta = process(initial_input, initial_weights)
+    h1_output, h1_weights, h1_delta = process(l1_output, l1_weights, l1_delta)
+    h2_output, h2_weights, h2_delta = process(h1_output, h1_weights, h1_delta)
+    h3_output, h3_weights, h3_delta = process(h2_output, h2_weights, h2_delta)
+    h4_output, h4_weights, h4_delta = process(h3_output, h3_weights, h3_delta)
+    h5_output, h5_weights, h5_delta = process(h4_output, h4_weights, h4_delta)
+    output = nonlin(h5_output * h5_weights)
 
-final = np.resize(output, (4, 1))
-print("Predicted:\n", final)
+print("Predicted:\n", output)
 print("\nActual:\n", Y)
